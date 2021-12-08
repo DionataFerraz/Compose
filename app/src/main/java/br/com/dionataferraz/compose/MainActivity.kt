52fffd32bb59cb9dc443c28c3bba84d7a9966de9
Dionata Ferraz <dionataferraz@8378-NB.local>
2021-12-08 13:53:37 -0300

    Lesson 3 Material Design: Dark theme

--- app/src/main/java/br/com/dionataferraz/compose/MainActivity.kt
+++ app/src/main/java/br/com/dionataferraz/compose/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.dionataferraz.compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -65,7 +66,12 @@ fun MessageCard(msg: Message) {
     }
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
     ComposeTutorialTheme {
