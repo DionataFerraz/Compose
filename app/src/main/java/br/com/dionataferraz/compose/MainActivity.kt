01fc1d668eb47eeab8cd95bf786f4d93a0fd7dd2
Dionata Ferraz <dionataferraz@8378-NB.local>
2022-01-05 12:48:50 -0300

    Lesson 4 Lists and animations: List of messages

--- app/src/main/java/br/com/dionataferraz/compose/MainActivity.kt
+++ app/src/main/java/br/com/dionataferraz/compose/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -18,6 +19,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.dionataferraz.compose.ui.theme.ComposeTutorialTheme
+import androidx.compose.foundation.lazy.items
+
+// Link to tutorial:
+// https://developer.android.com/jetpack/compose/tutorial?authuser=1&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fcompose%3Fauthuser%3D1%23article-https%3A%2F%2Fdeveloper.android.com%2Fjetpack%2Fcompose%2Ftutorial
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -78,5 +83,22 @@ fun PreviewMessageCard() {
         MessageCard(
             msg = Message("Dionata", "Hey, take a look at Jetpack Compose, it's great!")
         )
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
     }
 }
