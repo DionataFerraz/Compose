752a45bba9d1e0496f87a462ab8d71f0b77a71e7
Dionata <diouferraz@gmail.com>
2022-01-09 19:07:10 -0300

    Lesson 2: Starting a new compose project

--- app/src/main/java/br/com/dionataferraz/compose/MainActivity.kt
+++ app/src/main/java/br/com/dionataferraz/compose/MainActivity.kt
@@ -3,13 +3,36 @@ package br.com.dionataferraz.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.dionataferraz.compose.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            BasicsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
         }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+        Greeting("Android")
     }
 }
