f2072af54b9d34bc5799922998a709fd9dee405a
Dionata <diouferraz@gmail.com>
2022-01-09 20:48:14 -0300

    Lesson 5: Reusing composables

--- app/src/main/java/br/com/dionataferraz/compose/MainActivity.kt
+++ app/src/main/java/br/com/dionataferraz/compose/MainActivity.kt
@@ -18,12 +18,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
+    }
+}
+
+@Composable
+private fun MyApp() {
+    Surface(color = MaterialTheme.colors.background) {
+        Greeting("Android")
     }
 }
 
@@ -36,8 +40,8 @@ private fun Greeting(name: String) {
 
 @Preview(showBackground = true, name = "Text preview")
 @Composable
-fun DefaultPreview() {
+private fun DefaultPreview() {
     BasicsCodelabTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
